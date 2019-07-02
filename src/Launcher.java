5131d207b0dadd26e75f90b712638657664c1e9f
Jackie Chen <chenjackie4@gmail.com>
2019-07-01 21:07:23 -0700

    Changed Class Name

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,8 +1,9 @@
-public class Tester {
+public class Launcher {
     public static void main(String[] args) {
         int row = Matrix.prompt("Rows");
         int col = Matrix.prompt("Columns");
         Matrix m = new Matrix(row,col);
+        m.initialize();
         System.out.println(m);
     }
 }
